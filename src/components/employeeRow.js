0f218ea16382caad15078812320fed80ae9add3c
Red Fairy <agorelik2@gmail.com>
2020-10-26 11:45:49 -0400

    updated employeeRow component to display table details, employeeTable to display column headers

--- src/components/employeeRow.js
+++ src/components/employeeRow.js
@@ -2,6 +2,19 @@ import React from "react";
 // import "./EmployeeRow.css";
 
 function EmployeeRow(props) {
-    return ();
+  return (
+    <tr>
+      <td>
+        <img alt={props.last} src={props.photo} />
+      </td>
+      <td>{props.first}</td>
+      <td>{props.last}</td>
+      <td>{props.email}</td>
+      <td>{props.age}</td>
+      <td>
+        {props.city}, {props.state}
+      </td>
+    </tr>
+  );
 }
 export default EmployeeRow;
--- src/components/employeeTable.js
+++ src/components/employeeTable.js
@@ -2,7 +2,49 @@ import React from "react";
 import EmployeeRow from "./employeeRow";
 
 function EmployeeTable({ handleSort, filteredEmployees }) {
-    return ()
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th scope="col">Photo</th>
+          <th
+            scope="col"
+            value="first"
+            key="first"
+            onClick={() => {
+              handleSort();
+            }}
+          >
+            {/* <button type="button" onClick={props.handleSort}> */}
+            First
+            {/* </button> */}
+          </th>
+          <th value="last" scope="col">
+            <button type="button" onClick={handleSort}>
+              Last
+            </button>
+          </th>
+          <th scope="col">Email</th>
+          <th scope="col">Age</th>
+          <th scope="col">Location</th>
+        </tr>
+      </thead>
+      <tbody>
+        {filteredEmployees.map((employee) => (
+          <EmployeeRow
+            key={employee.id.value}
+            first={employee.name.first}
+            last={employee.name.last}
+            email={employee.email}
+            age={employee.dob.age}
+            city={employee.location.city}
+            state={employee.location.state}
+            photo={employee.picture.thumbnail}
+          />
+        ))}
+      </tbody>
+    </table>
+  );
 }
 
 export default EmployeeTable;
